f45b4c8bc1bc9e5eac74aeb6857b6639ba17b48b
Tyler Mack <plumberfrommercury+unity@gmail.com>
2016-09-18 09:22:49 -0700

    Added StartMenu scene, modified mainGame scene setup

---
+++ Assets/Scripts/ButtonScripts/StartGameButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartGameButton : MonoBehaviour {
+
+    public void OnClicked()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+}
